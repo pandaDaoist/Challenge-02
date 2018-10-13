e33fd326c42b4cdb5d439890521d959663c00de9
pandaDaoist <42003923+pandaDaoist@users.noreply.github.com>
2018-10-13 19:52:15 -0400

    Set-up

---
+++ 2D Mario World 1-1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb2d;
+    private int count;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+	}
+}
